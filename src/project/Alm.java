8343153d522e33a8237b874fcf5d59668916530e
david <david@LAPTOP-9BPRKTLN>
2019-09-27 23:46:52 -0500

    Merge origin/master
    Conflicts:
    	src/project/Administrador.java

--- src/project/Alm.java
+++ src/project/Alm.java
@@ -40,9 +40,9 @@ public class Alm {
         boolean[] confirmacion = new boolean[2];
         for(int i=0; i<personas.size;i++){
             if((personas.get(i).getCorreo().equals(correo)) && (personas.get(i).getContrasena().equals(contrasena))){
-                confirmacion[0]=true; 
+                confirmacion[0]=true;
+                confirmacion[1]=personas.get(i).getRol();
             }
-            confirmacion[1]=personas.get(i).getRol();
         }
         return confirmacion;
     }
